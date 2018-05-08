0f28edef037d0797b203a013236ae925882efa04
sergioramalli <iuriramalli@hotmail.com>
2018-05-08 02:49:49 +0100

    moved cgi files

--- cgi/output.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import cgi
-from lib import business_layer
-
-##This script will out all the needed information from the middle layer as requested from the front layer interactions 
-print('here')
